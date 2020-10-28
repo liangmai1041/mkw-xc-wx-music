668dcd297d55c4415bf5314139b6885fc4a9df07
lyx <651016060@qq.com>
2020-10-28 22:02:30 +0800

    char03 4 commit -- 详解wx-key (5/15)

--- miniprogram/pages/demo/demo.js
+++ miniprogram/pages/demo/demo.js
@@ -5,7 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    openid: ''
+    openid: '',
+    arr: ['wxml', 'js', 'wxss', 'json'],
+    arrObj: [
+      {id: 1, name: 'wxml'},
+      {id: 2, name: 'js'},
+      {id: 3, name: 'wxss'},
+      {id: 4, name: 'json'},
+    ],
+  },
+  sort() {
+    const length = this.data.arr.length
+    for(let i = 0; i < length; i++) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.arr[x]
+      this.data.arr[x] = this.data.arr[y]
+      this.data.arr[y] = temp
+    }
+    this.setData({
+      arr: this.data.arr
+    })
+  },
+  sortObj() {
+    const length = this.data.arrObj.length
+    for(let i = 0; i < length; i++) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.arrObj[x]
+      this.data.arrObj[x] = this.data.arrObj[y]
+      this.data.arrObj[y] = temp
+    }
+    this.setData({
+      arrObj: this.data.arrObj
+    })
   },
 
   /**
