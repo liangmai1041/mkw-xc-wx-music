bb8b0f8c1cab758dc65ee0408df10bb459cde795
lyx <651016060@qq.com>
2020-11-08 13:37:39 +0800

    char03 9 commit -- 歌单数据去重 (10/15)

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -11,8 +11,12 @@ const axios = require('axios')
 
 const URL = 'https://apis.imooc.com/personalized?icode=63C6A5B9F195BA7B'
 
+const playlistCollection = db.collection('playlist')
+
 // 云函数入口函数
 exports.main = async (event, context) => {
+  const list = await playlistCollection.get()
+
   const { data } = await axios.get(URL)
   if(data.code >= 1000) {
     console.log(data.msg)
@@ -20,13 +24,29 @@ exports.main = async (event, context) => {
   }
   const playlist = data.result
   
-  if(playlist.length > 0) {
-    await db.collection('playlist').add({
-      data: [...playlist]
+  const newData = []
+  for(let i = 0, len1 = playlist.length; i < len1; i++) {
+    let flag = true
+    for(let j = 0, len2 = list.data.length; j < len2; j++) {
+      if(playlist[i].id === list.data[j].id) {
+        flag = false
+        break
+      }
+    }
+    if(flag) {
+      newData.push(playlist[i])
+    }
+  }
+
+  if(newData.length > 0) {
+    await playlistCollection.add({
+      data: [...newData]
     }).then((res) => {
       console.log('插入成功')
     }).catch((err) => {
       console.error('插入失败')
     })
   }
+
+  return newData.length
 }
