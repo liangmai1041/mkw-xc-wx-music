96a81e6e698f10166048daf15fc7c078153d1562
lyx <651016060@qq.com>
2020-11-21 18:56:15 +0800

    char04 7 commit -- 拖拽进度条 (10/17)

--- miniprogram/components/progress-bar/progress-bar.js
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -3,6 +3,7 @@ let movableAreaWidth = 0
 let movableViewWidth = 0
 const backgroundAudioManager = wx.getBackgroundAudioManager()
 let currentSec = -1 // 当前秒数
+let duration = 0 // 当前歌曲的总时长，以秒为单位
 Component({
   /**
    * 组件的属性列表
@@ -34,6 +35,24 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onChange(e) {
+      // console.log(e);
+      // 拖动
+      if(e.detail.source === 'touch') {
+        this.data.progress = e.detail.x / (movableAreaWidth - movableViewWidth) * 100
+        this.data.movableDis = e.detail.x
+      }
+    },
+    onTouchEnd(e) {
+      console.log(e)
+      const currentTimeFmt = this._dateFormat(Math.floor(backgroundAudioManager.currentTime))
+      this.setData({
+        progress: this.data.progress,
+        movableDis: this.data.movableDis,
+        ['showTime.currentTime']: `${currentTimeFmt.min}:${currentTimeFmt.sec}`,
+      })
+      backgroundAudioManager.seek(duration * this.data.progress / 100)
+    },
     _getMovableDis() {
       const query = this.createSelectorQuery()
       query.select('.movable-area').boundingClientRect()
@@ -70,12 +89,12 @@ Component({
         }
       })
       backgroundAudioManager.onTimeUpdate(() => {
-        console.log('onTimeUpdate');
+        // console.log('onTimeUpdate');
         const currentTime = backgroundAudioManager.currentTime
         const duration = backgroundAudioManager.duration
         const sec = Math.floor(currentTime)
         if(sec !== currentSec) {
-          console.log(currentTime);
+          // console.log(currentTime);
           const currentTimeFmt = this._dateFormat(currentTime)
           this.setData({
             movableDis: (movableAreaWidth - movableViewWidth) * currentTime / duration,
@@ -97,7 +116,7 @@ Component({
       })
     },
     _setTime() {
-      const duration = backgroundAudioManager.duration
+      duration = backgroundAudioManager.duration
       console.log(duration);
       const durationFmt = this._dateFormat(duration)
       console.log(durationFmt);
