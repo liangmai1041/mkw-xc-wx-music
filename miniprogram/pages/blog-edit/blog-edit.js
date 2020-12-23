79d121da1658c7ed34359e01282361ffae91e813
lyx <651016060@qq.com>
2020-12-23 23:01:54 +0800

    char05 4 commit -- 发布博客页面布局与原生组件 (5/14)

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,11 +1,40 @@
 // pages/blog-edit/blog-edit.js
+
+// 输入文字最大的限制
+const MAX_WORDS_NUM = 140
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    // 输入的文字个数
+    wordsNum: 0,
+    footerBottom: 0,
+  },
 
+  onInput(e) {
+    console.log(e.detail.value);
+    let wordsNum = e.detail.value.length
+    if(wordsNum >= MAX_WORDS_NUM) {
+      wordsNum = `最大字数为${MAX_WORDS_NUM}`
+    }
+    this.setData({
+      wordsNum,
+    })
+  },
+  onFocus(e) {
+    // 模拟器获取的键盘高度为0
+    console.log(e);
+    this.setData({
+      footerBottom: e.detail.height,
+    })
+  },
+  onBlur(e) {
+    this.setData({
+      footerBottom: 0,
+    })
   },
 
   /**
